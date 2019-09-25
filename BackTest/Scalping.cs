d99f612b7a2d525a039f6edb832948b5a6b4749a
sangwoo park <cyberprophet@hanmail.net>
2019-09-25 17:58:10 +0900

    Make Neo Strategy

---
+++ BackTest/Scalping.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using ShareInvest.DataMining;
+using ShareInvest.EventHandler;
+using ShareInvest.Secondary;
+
+namespace ShareInvest.BackTest
+{
+    public class Scalping : Save
+    {
+        private IEnumerable ie;
+        private Situation s;
+
+        public event EventHandler<DayEvent> SendDay;
+        public event EventHandler<TickEvent> SendTick;
+
+        public void StartProgress(int param)
+        {
+            ie = new EnumerateDay();
+
+            foreach (string val in ie)
+            {
+                string[] arr = val.Split(',');
+
+                if (arr[1].Contains("-"))
+                    arr[1] = arr[1].Substring(1);
+
+                double price = double.Parse(arr[1]);
+
+                s = new MakeDay(arr[0].Substring(6, 2));
+
+                SendDay?.Invoke(this, new DayEvent(s.Check, price));
+            }
+            ie = new EnumerateTick();
+            act = new Action(() => Log(param));
+
+            foreach (string val in ie)
+            {
+                string[] arr = val.Split(',');
+
+                s = new TimeCheck();
+
+                if (arr[1].Contains("-"))
+                    arr[1] = arr[1].Substring(1);
+
+                double price = double.Parse(arr[1]);
+
+                SendTick?.Invoke(this, new TickEvent(s.Make(arr[0].Substring(9, 1)), arr[0], price, int.Parse(arr[2]), param));
+
+                s = new MakeDay(arr[0].Substring(4, 2));
+
+                SendDay?.Invoke(this, new DayEvent(s.Check, price));
+            }
+            act.BeginInvoke(act.EndInvoke, null);
+        }
+        protected const int tm = 250000;
+        protected const char separator = '\\';
+        protected const double commission = 3e-5;
+        protected const double margin = 7.5e-2;
+
+        protected double[] sma;
+        protected List<double> trend_width;
+        protected List<double> short_ema;
+        protected List<double> long_ema;
+        protected List<double> short_macd;
+        protected List<double> long_macd;
+        protected List<double> signal_macd;
+        protected BollingerBands b;
+        protected Indicator i;
+
+        protected double PurchasePrice
+        {
+            get
+            {
+                return purchase_price;
+            }
+            set
+            {
+                if (Math.Abs(amount) < Math.Abs(Quantity) && Quantity != 0)
+                {
+                    purchase_price = (purchase_price * Math.Abs(amount) + value) / Math.Abs(Quantity);
+                    amount = Quantity;
+                }
+            }
+        }
+        protected double CumulativeRevenue
+        {
+            get; set;
+        }
+        protected double Liquidation
+        {
+            get
+            {
+                return liquidation;
+            }
+            set
+            {
+                if (amount > Quantity && Quantity > -1)
+                {
+                    liquidation = value - purchase_price;
+                    amount = Quantity;
+                    CumulativeRevenue += Math.Round(liquidation * tm);
+                }
+                else if (amount < Quantity && Quantity < 1)
+                {
+                    liquidation = purchase_price - value;
+                    amount = Quantity;
+                    CumulativeRevenue += Math.Round(liquidation * tm);
+                }
+            }
+        }
+        protected static int TodayTrend
+        {
+            get; set;
+        }
+        protected long Revenue
+        {
+            get
+            {
+                return revenue;
+            }
+            set
+            {
+                revenue = value - Commission;
+            }
+        }
+        protected long TodayRevenue
+        {
+            get; set;
+        }
+        protected uint Commission
+        {
+            get; set;
+        }
+        protected ulong BasicAsset
+        {
+            get; set;
+        }
+        protected int Quantity
+        {
+            get; set;
+        }
+        protected uint TodayCommission
+        {
+            get; set;
+        }
+        protected uint TempCommission
+        {
+            get; set;
+        }
+        private int amount;
+        private long revenue;
+        private double purchase_price;
+        private double liquidation;
+    }
+}
---
+++ BackTest/Strategy/Tick.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ShareInvest.BackTest;
+using ShareInvest.EventHandler;
+using ShareInvest.Secondary;
+using ShareInvest.Secret;
+
+namespace ShareInvest.Strategy
+{
+    public class Tick : Scalping
+    {
+        public event EventHandler<SaveEvent> SendSave;
+
+        private readonly IArcanum ia;
+        private readonly string[] remaining =
+        {
+            "190911151959"
+        };
+        public Tick()
+        {
+            b = new BollingerBands();
+            sma = new double[b.MidPeriod];
+            trend_width = new List<double>(32768);
+            short_ema = new List<double>(32768);
+            long_ema = new List<double>(32768);
+            ia = new Arcanum();
+
+            SendSave += Storage;
+        }
+        public void Analysis(object sender, TickEvent e)
+        {
+            MakeMA(e.check, e.price);
+
+            int quotient, repeat = 0, quantity, sc = short_ema.Count, lc = long_ema.Count, wc = trend_width.Count;
+            double bo = 0, up = 0, ma = 0, sd, gap, width_gap, sb = sc > 1 ? short_ema[sc - 2] : 0, lb = lc > 1 ? long_ema[lc - 2] : 0, se = sc > 0 ? short_ema.Last() : 0, le = lc > 0 ? long_ema.Last() : 0;
+
+            i = lc > 0 ? new EMA() : new Indicator();
+
+            if (count > b.MidPeriod)
+            {
+                ma = b.MovingAverage(b.MidPeriod, sma);
+                sd = b.StandardDeviation(b.MidPeriod, ma, sma);
+                up = b.UpperLimit(ma, sd);
+                bo = b.BottomLine(ma, sd);
+            }
+            if (e.check == false)
+            {
+                short_ema[sc - 1] = i.Make(i.ShortPeriod, sc, e.price, sb);
+                long_ema[lc - 1] = i.Make(i.LongPeriod, lc, e.price, lb);
+                trend_width[wc - 1] = b.Width(ma, up, bo);
+            }
+            else
+            {
+                short_ema.Add(i.Make(i.ShortPeriod, sc, e.price, se));
+                long_ema.Add(i.Make(i.LongPeriod, lc, e.price, le));
+                trend_width.Add(b.Width(ma, up, bo));
+            }
+            gap = sc > 1 ? Trend() : 0;
+            width_gap = wc > b.MidPeriod ? TrendWidth() : 0;
+
+            if (e.volume > e.secret || e.volume < -e.secret)
+            {
+                quantity = Order(gap, width_gap);
+
+                if (Math.Abs(e.volume) < Math.Abs(e.volume + quantity) && !e.time.Substring(6, 4).Equals("1545"))
+                {
+                    ia.MaximumQuantity = ia.BasicAsset / (e.price * tm * margin);
+                    quotient = ia.Devide(e.volume);
+
+                    while (Math.Abs(Quantity + quantity) < ia.MaximumQuantity && Math.Abs(repeat) <= quotient)
+                        repeat += Operate(e.price, quantity);
+                }
+            }
+            if (e.time.Substring(6, 4).Equals("1545") || Array.Exists(remaining, o => o.Equals(e.time)))
+            {
+                while (Quantity != 0)
+                    Operate(e.price, Quantity > 0 ? -1 : 1);
+
+                Revenue = (long)CumulativeRevenue;
+                TodayCommission = Commission - TempCommission;
+
+                if (TodayCommission != 0)
+                    SendSave?.Invoke(this, new SaveEvent(e.time.Substring(0, 6), TodayCommission, Revenue - TodayRevenue, CumulativeRevenue - Commission));
+
+                TempCommission = Commission;
+                TodayRevenue = Revenue;
+            }
+        }
+        private int Operate(double price, int quantity)
+        {
+            Quantity += quantity;
+            Commission += (uint)Math.Truncate(price * tm * commission);
+            PurchasePrice = price;
+            Liquidation = price;
+
+            return quantity;
+        }
+        private int Order(double eg, double wg)
+        {
+            if (wg != 0 && !double.IsNaN(wg))
+                return eg + wg > 0 ? 1 : -1;
+
+            return 0;
+        }
+        private double TrendWidth()
+        {
+            int wc = trend_width.Count;
+
+            return trend_width[wc - 1] - trend_width[wc - 2];
+        }
+        private void MakeMA(bool check, double price)
+        {
+            if (check == true)
+                count++;
+
+            sma[Count] = price;
+        }
+        private double Trend()
+        {
+            int sc = short_ema.Count, lc = long_ema.Count;
+
+            return short_ema[sc - 1] - long_ema[lc - 1] - (short_ema[sc - 2] - long_ema[lc - 2]);
+        }
+        private int Count
+        {
+            get
+            {
+                return count % b.MidPeriod;
+            }
+        }
+        private int count = -1;
+    }
+}
