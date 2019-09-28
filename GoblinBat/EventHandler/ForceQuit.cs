9d16cc3f9fac4fe00088fb39fa7e058849f17743
sangwoo park <cyberprophet@hanmail.net>
2019-09-28 16:30:55 +0900

    Make EventHandler

---
+++ GoblinBat/EventHandler/ForceQuit.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ShareInvest.EventHandler
+{
+    public class ForceQuit : EventArgs
+    {
+        public int quit;
+
+        public ForceQuit(int quit)
+        {
+            this.quit = quit;
+        }
+    }
+}
