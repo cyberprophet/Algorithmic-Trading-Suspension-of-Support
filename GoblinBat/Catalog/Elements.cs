cab0e1d2c9b93d9c450b966640655d3245de24ef
sangwoo park <cyberprophet@hanmail.net>
2019-09-28 17:59:49 +0900

    Make Memorize

---
+++ GoblinBat/Catalog/Elements.cs
@@ -0,0 +1,24 @@
+﻿namespace ShareInvest.Catalog
+{
+    public class Elements
+    {
+        public Elements(string screen, string classification, int quantity, int position, double purchase)
+        {
+            this.screen = screen;
+            this.classification = classification;
+            this.quantity = quantity;
+            sRQName = string.Concat(position, ';', purchase, ';', classification, ';', quantity, ";New");
+        }
+        public Elements(string screen, string classification, string quantity, string position, string purchase)
+        {
+            this.screen = screen;
+            this.classification = classification;
+            this.quantity = int.Parse(quantity);
+            sRQName = string.Concat(position, ";", purchase, ";", classification, ";", quantity, ";ToMakeUp");
+        }
+        public int quantity;
+        public string sRQName;
+        public string screen;
+        public string classification;
+    }
+}
---
+++ GoblinBat/EventHandler/Memorize.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Text;
+
+namespace ShareInvest.EventHandler
+{
+    public class Memorize : EventArgs
+    {
+        public string date;
+        public string price;
+        public string volume;
+        public string sPrevNext;
+
+        public Memorize(StringBuilder sb)
+        {
+            string[] arr = sb.ToString().Split(',');
+
+            date = arr[2].Substring(2);
+            price = arr[0];
+            volume = arr[1];
+        }
+        public Memorize(string sPrevNext)
+        {
+            this.sPrevNext = sPrevNext;
+        }
+    }
+}
