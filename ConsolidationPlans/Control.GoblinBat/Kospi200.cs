4bad085c4a9cabad0e71d1f9259c10e7b4cad587
sangwoo park <cyberprophet@hanmail.net>
2019-10-07 15:28:19 +0900

    Procedure for Integrating Investment Items

---
+++ ConsolidationPlans/Control.GoblinBat/Kospi200.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShareInvest.Control
+{
+    public partial class Kospi200: UserControl
+    {
+        public Kospi200()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ConsolidationPlans/GoblinBat/GoblinBat.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShareInvest
+{
+    public partial class GoblinBat : Form
+    {
+        public GoblinBat()
+        {
+            InitializeComponent();
+        }
+    }
+}
