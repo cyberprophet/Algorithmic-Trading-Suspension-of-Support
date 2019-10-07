9a84f7e189a5a301439184f2a86469140ddfd892
sangwoo park <cyberprophet@hanmail.net>
2019-10-07 16:20:01 +0900

    Make DelayRequest

---
+++ ConsolidationPlans/DelayRequest.GoblinBat/Delay.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShareInvest.DelayRequest
+{
+    public class Delay
+    {
+        public static Delay GetInstance(int mSecond)
+        {
+            delay = mSecond;
+
+            if (request == null)
+                request = new Delay();
+
+            return request;
+        }
+        public void Run()
+        {
+            taskWorker.Start();
+        }
+        public void RequestTrData(Task task)
+        {
+            requestTaskQueue.Enqueue(task);
+        }
+        private Delay()
+        {
+            taskWorker = new Thread(delegate ()
+            {
+                while (true)
+                {
+                    try
+                    {
+                        while (requestTaskQueue.Count > 0)
+                        {
+                            requestTaskQueue.Dequeue().RunSynchronously();
+                            Thread.Sleep(delay);
+                        }
+                        Thread.Sleep(100);
+                    }
+                    catch (Exception exception)
+                    {
+                        Console.WriteLine(exception.Message);
+                    }
+                }
+            });
+        }
+        public static int delay = 210;
+        private static Delay request;
+        private readonly Thread taskWorker;
+        private readonly Queue<Task> requestTaskQueue = new Queue<Task>();
+    }
+}
