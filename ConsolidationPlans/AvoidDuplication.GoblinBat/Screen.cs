c4fcd45c7b84902ee07277b01e75942be7fc00a4
sangwoo park <cyberprophet@hanmail.net>
2019-10-07 15:54:37 +0900

    Make Common

---
+++ ConsolidationPlans/AvoidDuplication.GoblinBat/Screen.cs
@@ -0,0 +1,16 @@
+﻿namespace ShareInvest.AvoidDuplication
+{
+    public class Screen
+    {
+        public string GetScreen()
+        {
+            if (count < 9000)
+                count = 10000;
+
+            count--;
+
+            return count.ToString();
+        }
+        private static int count = 10000;
+    }
+}
