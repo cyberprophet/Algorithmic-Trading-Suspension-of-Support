b4e3d815dfd96fa3c6554a4283f4eb56676e19a1
sangwoo park <cyberprophet@hanmail.net>
2019-11-14 11:59:17 +0900

    Tasks to Create Backtesting Version for 64bit

--- IdentifyProcessors/Analysize.GoblinBat/Strategy.cs
+++ IdentifyProcessors/Analysize.GoblinBat/Strategy.cs
@@ -1,4 +1,6 @@
-﻿using ShareInvest.Interface.x64;
+﻿using System.Collections.Generic;
+using ShareInvest.Interface.x64;
+using ShareInvest.SecondaryIndicators.x64;
 
 namespace ShareInvest.Analysize.x64
 {
@@ -6,8 +8,22 @@ namespace ShareInvest.Analysize.x64
     {
         public Strategy(IStrategy st)
         {
+            ema = new EMA();
+            shortEMA = new List<double>(32768);
+            longEMA = new List<double>(32768);
+            shortDay = new List<double>(512);
+            longDay = new List<double>(512);
+            shortTick = new List<double>(2097152);
+            longTick = new List<double>(2097152);
             this.st = st;
         }
         private readonly IStrategy st;
+        private readonly EMA ema;
+        private readonly List<double> shortEMA;
+        private readonly List<double> longEMA;
+        private readonly List<double> shortDay;
+        private readonly List<double> longDay;
+        private readonly List<double> shortTick;
+        private readonly List<double> longTick;
     }
 }
