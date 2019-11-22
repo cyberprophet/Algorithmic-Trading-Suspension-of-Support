6a4fa2812d8d0c0d6b0f652fea1f9d14ab08b224
sangwoo park <cyberprophet@hanmail.net>
2019-11-22 22:08:29 +0900

    Make Install Controller

---
+++ Publish.GoblinBat/Guide.GoblinBat/GoblinBat.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using IWshRuntimeLibrary;
+
+namespace ShareInvest.Guide
+{
+    public partial class GoblinBat : UserControl
+    {
+        public GoblinBat()
+        {
+            InitializeComponent();
+            SetRoute(new WshShell());
+        }
+        private void SetRoute(WshShell ws)
+        {
+            string[] temp;
+            IWshShortcut sc;
+
+            try
+            {
+                foreach (string file in Directory.GetFiles(Environment.CurrentDirectory, "*.exe", SearchOption.AllDirectories))
+                {
+                    temp = file.Split('\\');
+                    temp = temp[temp.Length - 1].Split('.');
+
+                    if (Array.Exists(repeat, o => o.Equals(temp[0])))
+                    {
+                        sc = (IWshShortcut)ws.CreateShortcut(string.Concat(desk, @"\", temp[0], ".lnk"));
+                        sc.TargetPath = file;
+                        sc.Description = file.Contains(trading) ? "GoblinBatTraing" : "GoblinBatBackTesting";
+                        sc.IconLocation = file.Replace("exe", "ico");
+                        sc.Save();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private readonly string[] repeat = { trading, backTesting };
+        private readonly string desk = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        private const string trading = "Kospi200";
+        private const string backTesting = "BackTesting";
+    }
+}
---
+++ Publish.GoblinBat/Install.GoblinBat/Install.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using ShareInvest.C2010;
+using ShareInvest.C2012;
+using ShareInvest.Guide;
+using ShareInvest.NetFramework;
+using ShareInvest.OpenAPI;
+
+namespace ShareInvest.Install
+{
+    public partial class Install : Form
+    {
+        public Install()
+        {
+            InitializeComponent();
+            button2010.Click += ButtonClick;
+            button2012.Click += ButtonClick;
+            buttonDOTNETFramework.Click += ButtonClick;
+            buttonGoblinBat.Click += ButtonClick;
+            buttonOpenAPI.Click += ButtonClick;
+            MessageBox.Show("The GoblinBat Works Correctly when\nAll Elements are Installed.\n\nClick on a Program\nthat is Not Installed.\n\nThe GoblinBat must be Installed Last.", "Notice", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        private void ButtonClick(object sender, EventArgs e)
+        {
+            if (sender.Equals(buttonOpenAPI))
+            {
+                new Setup().StartProgress();
+                buttonOpenAPI.ForeColor = Color.Maroon;
+
+                return;
+            }
+            if (sender.Equals(button2010))
+            {
+                new MSVC2010().StartProgress();
+                button2010.ForeColor = Color.Maroon;
+
+                return;
+            }
+            if (sender.Equals(button2012))
+            {
+                new MSVC2012().StartProgress();
+                button2012.ForeColor = Color.Maroon;
+
+                return;
+            }
+            if (sender.Equals(buttonDOTNETFramework))
+            {
+                new Net().StartProgress();
+                buttonDOTNETFramework.ForeColor = Color.Maroon;
+
+                return;
+            }
+            if (sender.Equals(buttonGoblinBat))
+            {
+                SuspendLayout();
+                tableLayoutPanel.Hide();
+                WindowState = FormWindowState.Minimized;
+                using GoblinBat gb = new GoblinBat();
+                Controls.Add(gb);
+                SetVisibleCore(false);
+                gb.Dock = DockStyle.Fill;
+                WindowState = FormWindowState.Maximized;
+                Opacity = 0.95;
+                ResumeLayout();
+
+                if (DialogResult.OK.Equals(MessageBox.Show("The Installation is Complete.\n\nPlease Register Your OpenAPI.", "Notice", MessageBoxButtons.OKCancel, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1)))
+                    ShowDialog();
+
+                Close();
+                Dispose();
+                Application.Exit();
+
+                return;
+            }
+        }
+    }
+}
