355749950f4bece50493e8dc5256ca702aff884d
sangwoo park <cyberprophet@hanmail.net>
2019-11-24 20:21:57 +0900

    Finalization Stages for Deployment

--- ClientDennyKospi200/OpenAPI.GoblinBat/ConfirmOrder.cs
+++ ClientDennyKospi200/OpenAPI.GoblinBat/ConfirmOrder.cs
@@ -31,7 +31,7 @@ namespace ShareInvest.OpenAPI
             if (e.Confirm != null && e.Confirm.Equals(message))
             {
                 if (TimerBox.Show(string.Concat(message, "\n\nDo You Want to Continue with BackTesting??\n\nIf You don't Want to Proceed,\nPress 'No'.\n\nAfter 35 Seconds the Program is Terminated."), "Notice", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button1, 35752).Equals((DialogResult)6))
-                    Process.Start("BackTesting.exe");
+                    Process.Start(string.Concat(Application.StartupPath, @"\BackTesting.exe"));
 
                 SendTab?.Invoke(this, new Mining(9));
             }
