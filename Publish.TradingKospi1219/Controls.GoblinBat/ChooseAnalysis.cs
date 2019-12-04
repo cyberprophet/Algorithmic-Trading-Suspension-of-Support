7244e44867eab092d8ffbf64dd86e4c54531622e
sangwoo park <cyberprophet@hanmail.net>
2019-12-04 11:42:21 +0900

    Clean Up the Code

--- Publish.TradingKospi1219/Controls.GoblinBat/ChooseAnalysis.cs
+++ Publish.TradingKospi1219/Controls.GoblinBat/ChooseAnalysis.cs
@@ -5,7 +5,6 @@ using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Windows.Forms;
-using ShareInvest.AutoMessageBox;
 using ShareInvest.Const;
 using ShareInvest.EventHandler;
 using ShareInvest.FindByName;
