e9283342dc87c64c1a5f8ef39e0e79dac5dc39af
sangwoo park <cyberprophet@hanmail.net>
2019-12-11 10:54:43 +0900

    Make Automatic GoblinBat Process

---
+++ Publish.Automatic.GoblinBat/Automatic.GoblinBat/GoblinBat.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ShareInvest.Progress;
+
+namespace ShareInvest.Automatic
+{
+    static class GoblinBat
+    {
+        static void Main()
+        {
+            new Select().StartProcess(DateTime.Now.Hour < 16 ? @"\Kospi200.exe" : @"\BackTesting.exe");
+            Environment.Exit(0);
+        }
+    }
+}
---
+++ Publish.Automatic.GoblinBat/Progress.GoblinBat/Select.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace ShareInvest.Progress
+{
+    public class Select : Form
+    {
+        public void StartProcess(string process)
+        {
+            Process.Start(string.Concat(Application.StartupPath, process));
+            Close();
+            Dispose();
+            Application.ExitThread();
+        }
+    }
+}
