2b45930d3f8172be7f76ea8e80f54fa484296d6c
sangwoo park <cyberprophet@hanmail.net>
2020-01-03 17:40:14 +0900

    Make GoblinBat.ShareInvest.Net

---
+++ GoblinBat.ShareInvest.Net/Net.GoblinBat.June.2020/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Net.GoblinBat.June._2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
