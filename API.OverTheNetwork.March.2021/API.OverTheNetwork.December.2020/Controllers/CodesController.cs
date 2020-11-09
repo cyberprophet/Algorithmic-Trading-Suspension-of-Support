2285bd9a4e84db118dfa1ffe0edeed5cc331de05
sangwoo park <cyberprophet@hanmail.net>
2020-11-10 00:30:45 +0900

    Attempt to connect Communication between API & Client

---
+++ API.OverTheNetwork.March.2021/API.OverTheNetwork.December.2020/Controllers/CodesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using ShareInvest.Catalog.Models;
+
+namespace ShareInvest.Controllers
+{
+    [ApiController, Route(Security.route), Produces(Security.produces)]
+    public class CodesController : ControllerBase
+    {
+        [HttpPut, ProducesResponseType(StatusCodes.Status204NoContent), ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<IActionResult> PutContextAsync([FromBody] Codes codes)
+        {
+            
+            return NoContent();
+        }
+    }
+}
---
+++ API.OverTheNetwork.March.2021/Client.GoblinBat/GoblinBat.cs
@@ -0,0 +1,44 @@
+﻿using System.Threading;
+
+using RestSharp;
+
+namespace ShareInvest.Client
+{
+    public sealed class GoblinBat
+    {
+        public static GoblinBat GetInstance(dynamic key)
+        {
+            if (Client == null)
+                Client = new GoblinBat(key);
+
+            return Client;
+        }
+        public static GoblinBat GetInstance()
+        {
+            if (Client == null)
+                Client = new GoblinBat();
+
+            return Client;
+        }
+        static GoblinBat Client
+        {
+            get; set;
+        }
+        GoblinBat()
+        {
+            security = new Security(int.MinValue);
+            client = new RestClient(security.Url)
+            {
+                Timeout = -1
+            };
+            source = new CancellationTokenSource();
+        }
+        GoblinBat(dynamic key)
+        {
+
+        }
+        readonly CancellationTokenSource source;
+        readonly Security security;
+        readonly IRestClient client;
+    }
+}
