9ad3706ec262e3fc0e522fd174052aa7932abdd1
sangwoo park <cyberprophet@hanmail.net>
2020-11-10 22:50:05 +0900

    Complete the Code in consideration of Security when entering the Program

---
+++ API.OverTheNetwork.March.2021/Install.GoblinBat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ShareInvest
+{
+    class Program
+    {
+        static void Main()
+        {
+            var key = new Verify.KeyDecoder().ProductKeyFromRegistry;
+            var security = new Security(key);
+
+            if (string.IsNullOrEmpty(key) == false && security.GrantAccess && security.CheckTheSystemPeriodically(DateTime.Now))
+                security.StartProgress();
+
+            GC.Collect();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
