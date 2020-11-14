ca713814c2d8acdfd1d4fb9727c7f55fdada8a2a
sangwoo park <cyberprophet@hanmail.net>
2020-11-14 15:43:41 +0900

    API update automation Code Implementation

---
+++ API.SeparateSystem.September.2020/CoreAPI.AutomaticUpgrade.Install/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ShareInvest
+{
+    class Program
+    {
+        static bool IsDebug
+        {
+            get; set;
+        }
+        static void Main()
+        {
+            ChangePropertyToDebugMode();
+
+            if (IsDebug)
+                Security.ChooseTheInstallationPath(Console.ReadLine());
+
+            else
+            {
+
+            }
+            GC.Collect();
+            Process.GetCurrentProcess().Kill();
+        }
+        [Conditional("DEBUG")]
+        static void ChangePropertyToDebugMode() => IsDebug = true;
+    }
+}
