75deee25dba61904b897bf2a61af83d1c1bd6fa0
sangwoo park <cyberprophet@hanmail.net>
2020-11-14 20:25:44 +0900

    Learn to communicate through Socket

---
+++ API.SeparateSystem.September.2020/CoreAPI.AutomaticUpgrade.Install/Client.cs
@@ -0,0 +1,15 @@
+﻿namespace ShareInvest
+{
+    class Client
+    {
+        internal string TransmitTheDataToUpdate()
+        {
+            return message;
+        }
+        internal Client()
+        {
+
+        }
+        const string message = @"Installation is complete on {0}.\a";
+    }
+}
--- API.SeparateSystem.September.2020/CoreAPI.AutomaticUpgrade.Install/Program.cs
+++ API.SeparateSystem.September.2020/CoreAPI.AutomaticUpgrade.Install/Program.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Runtime.Versioning;
 
 namespace ShareInvest
 {
@@ -9,16 +13,39 @@ namespace ShareInvest
         {
             get; set;
         }
+        const string core_api = ".zip";
+        const string path = @"C:\ShareInvest\";
+        [STAThread, SupportedOSPlatform("windows")]
         static void Main()
         {
             ChangePropertyToDebugMode();
 
-            if (IsDebug)
-                Security.ChooseTheInstallationPath(Console.ReadLine());
+            if (IsDebug && Security.IsAdministrator)
+            {
+                string name = Console.ReadLine(), version = string.Empty;
+                var address = Security.ChooseTheInstallationPath(name);
+                var files = new List<string>();
 
+                foreach (var file in new DirectoryInfo(address.Item2).GetFiles("*.*", SearchOption.AllDirectories))
+                {
+                    if (file.Name.Equals("CoreAPI.exe"))
+                    {
+                        var temp = FileVersionInfo.GetVersionInfo(file.FullName).FileVersion.Replace(".", string.Empty)[2..];
+                        version = string.Concat(temp[4..], temp[0..^4]);
+                    }
+                    files.Add(file.FullName);
+                }
+                using (var zip = ZipFile.Open(string.Concat(path, name, version, core_api), ZipArchiveMode.Create))
+                    foreach (var file in files)
+                        zip.CreateEntryFromFile(file, Path.GetFileName(file), CompressionLevel.Optimal);
+
+                Console.WriteLine(new Client().TransmitTheDataToUpdate(), address);
+            }
             else
             {
+                new Server();
 
+                return;
             }
             GC.Collect();
             Process.GetCurrentProcess().Kill();
