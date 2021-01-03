c588a9d878c2a6433a47f5bab9b7f15025527066
sangwoo park <cyberprophet@hanmail.net>
2021-01-03 17:12:49 +0900

    Market starts an hour late at the beginning of the year.

---
+++ API.Publish.OverTheNetwork.June.2021/Install.API/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ShareInvest
+{
+    class Program
+    {
+        static void Main()
+        {
+            if (Security.UpdateToVersion())
+                StartProgress();
+
+            else
+                Process.Start(Security.StartInfo);
+        }
+        static void StartProgress()
+        {
+            new Security(Verify.KeyDecoder.ProductKeyFromRegistry).StartProgress();
+            GC.Collect();
+            Process.GetCurrentProcess().Kill();
+        }
+    }
+}
---
+++ API.Publish.OverTheNetwork.June.2021/ShareInvest.API/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace ShareInvest
+{
+	class Program
+	{
+		static void Main()
+		{
+			var security = new dynamic[] { Security.Commands, Security.Compress };
+
+			for (int i = 0; i < security.Length; i++)
+				ChooseTheInstallationPath(security[i]);
+
+			if (Security.SendUpdateToFile().Result is string file)
+				File.Delete(file);
+		}
+		static void ChooseTheInstallationPath(dynamic param)
+		{
+			using var process = new Process
+			{
+				StartInfo = new ProcessStartInfo
+				{
+					FileName = cmd,
+					CreateNoWindow = true,
+					UseShellExecute = false,
+					RedirectStandardInput = true,
+					RedirectStandardError = true,
+					RedirectStandardOutput = true,
+					WorkingDirectory = param.Item1
+				}
+			};
+			if (process.Start())
+			{
+				process.StandardInput.Write(param.Item2 + Environment.NewLine);
+				process.StandardInput.Close();
+				Console.WriteLine(process.StandardOutput.ReadToEnd());
+				process.WaitForExit();
+			}
+		}
+		const string cmd = @"cmd";
+	}
+}
