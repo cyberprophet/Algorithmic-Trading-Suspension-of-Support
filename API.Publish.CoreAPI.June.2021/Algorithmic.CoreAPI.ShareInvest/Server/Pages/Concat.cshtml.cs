bf06fcd577f04459a9013493358edd5ed8a49508
sangwoo park <cyberprophet@hanmail.net>
2021-05-17 19:24:44 +0900

    Change Server Port

---
+++ API.Publish.CoreAPI.June.2021/Algorithmic.CoreAPI.ShareInvest/Server/Pages/Concat.cshtml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ShareInvest.Pages
+{
+	public class ConcatModel : PageModel
+	{
+		public List<Tuple<string, byte[]>> Files
+		{
+			get; set;
+		}
+		public IActionResult OnGet()
+		{
+			Files = new List<Tuple<string, byte[]>>();
+
+			foreach (var file in resources)
+				Files.Add(new Tuple<string, byte[]>(file.Key, file.Value));
+
+			return Page();
+		}
+		public ConcatModel() => resources = new()
+		{
+			{
+				nameof(Properties.Resources._1)[1..],
+				Properties.Resources._1
+			},
+			{
+				nameof(Properties.Resources._2)[1..],
+				Properties.Resources._2
+			},
+			{
+				nameof(Properties.Resources._3)[1..],
+				Properties.Resources._3
+			}
+		};
+		readonly Dictionary<string, byte[]> resources;
+	}
+}
